a8048c52faaee6b17df7498352993138962abc25
DayaneArnaud <dayanearnaud@gmail.com>
2023-12-15 19:49:55 -0300

    First question solved

---
+++ questoes.js
@@ -0,0 +1,18 @@
+//Dayane Arnaud Anadon Figueiredo id: 1087026
+
+// Primeira questão:
+let produto = 1;
+let fator = 3;
+
+while (fator <= 10) {
+  produto *= fator;
+  fator += 2;
+}
+// prod: 1*3=3 | fator: 3+2=5
+// prod: 3*5=15 | fator: 5+2=7
+// prod: 15*7=105 | fator: 7+2=9
+// prod: 105*9=945 e fator: 9+2=11 break
+
+// RESPOSTA CORRETA LETRA C) 945
+
+///////////////////////////////////////////////////////////////////////////
